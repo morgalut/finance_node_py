5c84b0596dfbf85547539d6787d5fb2ab1cc0d71
BenFuchs <benayahcode@gmail.com>
2024-08-23 18:14:39 +0300

    feat: added register and login function with basic JWT

--- personal-finance-backend/controllers/userController.js
+++ personal-finance-backend/controllers/userController.js
@@ -1,19 +1,52 @@
 const User = require('../models/user');
 
+require('dotenv').config(); // Load environment variables from .env file
+const jwt = require('jsonwebtoken'); // Import jsonwebtoken for token handling
+
+
+const secretKey = process.env.SECRET_KEY;
+
+
 class UserController {
     static async register(req, res) {
         try {
             const user = new User(req.body);
             await user.save();
             res.status(201).send(user);
+            console.log("new user registered")
         } catch (error) {
             res.status(400).send(error);
         }
     }
 
     static async login(req, res) {
-        // Implement login logic
+        try {
+            const { username, password } = req.body;
+
+            const user = await User.findOne({ username });
+
+            if (!user) {
+                return res.status(404).send({ message: "User does not exist" });
+            }
+
+            if (user.password !== password) {
+                return res.status(400).send({ message: "One of the inputs is wrong" });
+            }
+
+            // Generate a JWT token
+            try {
+                const token = jwt.sign({ userId: user._id, username: user.username }, secretKey, { expiresIn: '1h' });
+                res.status(200).send({ message: "User exists", token });
+            } catch (jwtError) {
+                console.error("JWT signing error:", jwtError);
+                res.status(500).send({ message: "Error generating token" });
+            }
+        } catch (error) {
+            console.error("Login error:", error);
+            res.status(500).send({ message: "Server error", error });
+        }
     }
+     
 }
 
 module.exports = UserController;
--- personal-finance-backend/models/user.js
+++ personal-finance-backend/models/user.js
@@ -4,6 +4,8 @@ const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    gender: {type: String, required: true},
+    age: {type: Number, required: true},
 });
 
 module.exports = mongoose.model('User', UserSchema);
